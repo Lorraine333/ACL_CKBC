655f3aaf28ff5040f50e72fb8118934766306969
lorraine <abbeyli92@gmail.com>
2018-11-18 20:27:13 -0500

    change default running script using the full training data

--- bilinear_ce/train.sh
+++ bilinear_ce/train.sh
@@ -1,3 +1,3 @@
-python train.py 200 150 0.001 0.001 0.01 200 RAND 30 0.01
-python train.py 200 150 0.001 0.001 0.01 200 MIX 30 0.01
-python train.py 200 150 0.001 0.001 0.01 200 MAX 30 0.01
+python train.py 200 150 0.001 0.001 0.01 200 RAND 30 1
+python train.py 200 150 0.001 0.001 0.01 200 MIX 30 1
+python train.py 200 150 0.001 0.001 0.01 200 MAX 30 1
--- dnn_hingeloss/train.sh
+++ dnn_hingeloss/train.sh
@@ -1,3 +1,3 @@
-python train.py 200 200 rectify 0.001 0 200 0.01 1000 RAND 0.01
-python train.py 200 200 rectify 0.001 0 200 0.01 1000 MIX 0.01
-python train.py 200 200 rectify 0.001 0 200 0.01 1000 MAX 0.01
+python train.py 200 200 rectify 0.001 0 200 0.01 1000 RAND 1
+python train.py 200 200 rectify 0.001 0 200 0.01 1000 MIX 1
+python train.py 200 200 rectify 0.001 0 200 0.01 1000 MAX 1
